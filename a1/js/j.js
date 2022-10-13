b27f0e22dee73850300bb10e6034ec5007842b14
kayan9896 <a4ssgit@hotmail.com>
2022-10-13 13:56:46 -0700

    datetime

--- a1/js/j.js
+++ a1/js/j.js
@@ -1,28 +1,34 @@
-const btn = document.querySelector('button');
+const btn = document.querySelector("#dl");
+const tbtn=document.querySelector("section");
+const txt=document.querySelector("#log");
 
 let de=localStorage.getItem("default");
 if(de=="dark"){
   document.querySelector('body').classList.toggle("darkmode");
-  document.querySelector('button').classList.toggle("darkbut");
+  document.querySelector("#dl").classList.toggle("darkbut");
   btn.innerText="light";
-}else{
-  document.querySelector('body').classList.remove("darkmode");
-  document.querySelector('button').classList.remove("darkbut");
-  btn.innerText="dark";
 }
 function changeBackground() {
   if(btn.innerText=="dark"){
     document.querySelector('body').classList.toggle("darkmode");
-    document.querySelector('button').classList.toggle("darkbut");
+    document.querySelector("#dl").classList.toggle("darkbut");
     btn.innerText="light";
     localStorage.setItem("default", "dark");
   }else{
     document.querySelector('body').classList.remove("darkmode");
-    document.querySelector('button').classList.remove("darkbut");
+    document.querySelector("#dl").classList.remove("darkbut");
     btn.innerText="dark";
     localStorage.setItem("default", "light");
   }
   
 }
+function displayDateTime(){
+  console.log(this.value);
+  if(this.value=="tm"){
+    const d=new Date();
+    log.innerText=d.getTime();
+  }else{log.innerText=Date.now()}
+}
 
 btn.addEventListener('click', changeBackground);
+tbtn.addEventListener('click', displayDateTime);
