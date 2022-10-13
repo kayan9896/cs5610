7b68b67376016f8242ba868f044fbf9cadfb114c
kayan9896 <a4ssgit@hotmail.com>
2022-10-13 12:47:10 -0700

    localstorage

--- a1/js/j.js
+++ a1/js/j.js
@@ -1,18 +1,26 @@
 const btn = document.querySelector('button');
 
-
+let de=localStorage.getItem("default");
+if(de=="dark"){
+  document.querySelector('body').classList.toggle("darkmode");
+  document.querySelector('button').classList.toggle("darkbut");
+  btn.innerText="light";
+}else{
+  document.querySelector('body').classList.remove("darkmode");
+  document.querySelector('button').classList.remove("darkbut");
+  btn.innerText="dark";
+}
 function changeBackground() {
   if(btn.innerText=="dark"){
     document.querySelector('body').classList.toggle("darkmode");
-    btn.style.color="white";
-    btn.style.backgroundColor="transparent";
+    document.querySelector('button').classList.toggle("darkbut");
     btn.innerText="light";
-    
+    localStorage.setItem("default", "dark");
   }else{
     document.querySelector('body').classList.remove("darkmode");
-    btn.style.color="black";
+    document.querySelector('button').classList.remove("darkbut");
     btn.innerText="dark";
-    
+    localStorage.setItem("default", "light");
   }
   
 }
